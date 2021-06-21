396ff06d7b5c1f943842f01a9dde3521c5765836
Anna Zocher <anna.zocher@gmail.com>
2021-06-21 11:33:47 -0700

    initial hello world

---
+++ src/ScoreKeeper.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class ScoreKeeper extends Component {
+    render() {
+        return (
+            <h1>Hello, World 👋</h1>
+        )
+    }
+}
