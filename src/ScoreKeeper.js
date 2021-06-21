5bb1d5fdcf94e20816ad8f4a7e1ff6171d7deba5
Anna Zocher <anna.zocher@gmail.com>
2021-06-21 11:44:34 -0700

    added state for scores

--- src/ScoreKeeper.js
+++ src/ScoreKeeper.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react'
 
 export default class ScoreKeeper extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+            score1: 0,
+            score2: 0
+        }
+    }
+
     render() {
         return (
-            <h1>Hello, World 👋</h1>
+            <div>
+                <div>
+                    <h3>Score Player 1:</h3>
+                    <p>{this.state.score1}</p>
+                </div>
+                <div>
+                    <h3>Score Player 2:</h3>
+                    <p>{this.state.score2}</p>
+                </div>
+            </div>
         )
     }
 }
